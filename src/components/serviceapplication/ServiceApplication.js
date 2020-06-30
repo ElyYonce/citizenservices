427887dd9ed11f20484fc70a6896d1143677724f
ElyYonce <elyyonce11@gmail.com>
2020-06-30 18:42:35 -0400

    call create case service in service application

--- src/components/serviceapplication/ServiceApplication.js
+++ src/components/serviceapplication/ServiceApplication.js
@@ -15,17 +15,26 @@ class ServiceApplication extends React.Component {
                  };
     }
     
-    
+    getServiceApplication = async (event) => {
+        const resp = await caseService.createCase(this.props.service.WorkClass, {});
+        this.setState({data: resp, isLoading: false});
+        console.log(this.state.data);
+    };
+
     //runs when the component is being loaded
     componentDidMount(){
         this.setState({service:this.props.service})
-        console.log(this.props.service);
+        //console.log(this.props.service);
+        this.setState({data: this.getServiceApplication()});
     }
     
     render(){
         const { data, isLoading, serviceName,service } = this.state;
         return (
+          <div>
            <h3>{service.ServiceName} Application</h3>
+           {isLoading ? "Loading..." : this.state.data.ID}
+          </div> 
         );
     }
 }
